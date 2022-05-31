fa91738bb062171942f161120ce3ba149309a359
ajbt200128 <austin@r2c.dev>
2022-05-31 14:01:21 -0700

    Release

--- scripts/build-wheels-m1.sh
+++ scripts/build-wheels-m1.sh
@@ -1,7 +1,7 @@
 # MUST be zsh or else python will label the wheel as x86
 #!/usr/bin/zsh
 set -e
-pip install setuptools wheel
-cd semgrep && python setup.py sdist bdist_wheel
+pip3 install setuptools wheel
+cd semgrep && python3 setup.py sdist bdist_wheel
 # Zipping for a stable name to upload as an artifact
 zip -r dist.zip dist
