f349b81e74c8d8a75d792a380faead0cc3cfdeb8
Austin Theriault <github@cutedogs.org>
2022-05-31 15:19:47 -0700

    Update osx-m1-release.sh

--- scripts/osx-m1-release.sh
+++ scripts/osx-m1-release.sh
@@ -1,13 +1,21 @@
+#!/usr/bin/env bash
 # Because we're running this on a remote machine, we don't want to reinstall
 # everything every time
-#!/usr/bin/env bash
+set -e
 opam switch 4.12.0;
 git submodule update --init --recursive --depth 1
 
 eval "$(opam env)"
 
+rm /usr/local/opt/pcre/lib/libpcre.1.dylib
+
 make setup
 make config
+
+# Remove dynamically linked libraries to force MacOS to use static ones
+# This needs to be done after make setup but before make build-*
+rm /usr/local/lib/libtree-sitter.0.0.dylib
+rm /usr/local/lib/libtree-sitter.dylib
 
 make build-core
 
